9b7ca3eeb3b8a7f2d052d65cb7535606da1932fc
Daniel Vermeulen <34882@ma-web.nl>
2023-06-29 11:02:16 +0200

    form

---
+++ eindopdracht/login.php
@@ -0,0 +1,26 @@
+<?php
+$dbname = "formulier.db";
+
+$login_username = $_POST['username'];
+$login_password = $_POST['password'];
+
+try {
+    $conn = new PDO("sqlite:$dbname");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $selectQuery = "SELECT * FROM users WHERE username = :username";
+    $stmt = $conn->prepare($selectQuery);
+    $stmt->bindParam(':username', $login_username);
+    $stmt->execute();
+
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && $user['password'] === $login_password) {
+        echo "Login successful!";
+    } else {
+        echo "Login doesn't match!";
+    }
+} catch (PDOException $e) {
+    die("Error connecting to the database: " . $e->getMessage());
+}
+?>
